7f54488984ad2a37007db0d29e89a27056dee756
yogeendran <yogeendran.jayatheerthan@wipro.com>
2017-01-11 08:37:17 +0000

    hacker

---
+++ app.js
@@ -0,0 +1,27 @@
+var http=require('http');
+var fs=require('fs');
+
+function error(response){
+response.writeHead(404, {"content-type":"text/plain"});
+response.write("Error occured in the page");
+response.end();
+}
+
+function onrequest(request,response){
+if(request.method == 'GET' && request.url == '/one')
+{
+	response.writeHead(200, {"content-type":"text/html"});
+	fs.createReadStream("./one.json").pipe(response);
+}
+else if(request.method == 'GET' && request.url == '/two')
+{
+	response.writeHead(200, {"content-type":"text/html"});
+	fs.createReadStream("./two.json").pipe(response);
+}
+else
+{
+	error(response); 
+}
+}
+http.createServer(onrequest).listen(8888);
+console.log("Server is now on"); 
