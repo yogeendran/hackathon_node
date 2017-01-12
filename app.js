d210392a0f107307fc83c0fd7074d0e2bd26bc6e
yogeendran <yogeendran.jayatheerthan@wipro.com>
2017-01-12 14:32:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var http=require('http');
 var fs=require('fs');
+var one=require('./one.js');
+var two=require('./two.js');
+var three=require('./three.js');
+var four=require('./four.js');
 
 function error(response){
 response.writeHead(404, {"content-type":"text/plain"});
@@ -10,13 +14,21 @@ response.end();
 function onrequest(request,response){
 if(request.method == 'GET' && request.url == '/one')
 {
-	response.writeHead(200, {"content-type":"text/json"});
-	fs.createReadStream("./one.json").pipe(response);
+	one.onrequest(request,response);
 }
+
+else if(request.method == 'POST' && request.url == '/three')
+{
+	three.onrequest(request,response);
+}
+
 else if(request.method == 'GET' && request.url == '/two')
 {
-	response.writeHead(200, {"content-type":"text/json"});
-	fs.createReadStream("./two.json").pipe(response);
+	two.onrequest(request,response);
+}
+else if(request.method == 'GET' && request.url == '/four/:country')
+{
+	four.finddata(request,response);
 }
 else
 {
